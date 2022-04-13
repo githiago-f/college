dfa5708c92cbff0081ba762eaccfeb4dbd01a16f
githiago-f <tfarias@protonmail.com>
2022-04-12 22:34:07 -0300

    subindo

--- DSI/malloc/ex3.c
+++ DSI/malloc/ex3.c
@@ -11,7 +11,7 @@ Cada contato é criado dinamicamente ocupando as posições no vetor sequencialm
 Não esqueça de liberar a memória no final do programa, antes de sair.
 */
 
-#define MAX 3
+#define MAX 100
 
 typedef struct {
     char nome[80];
@@ -21,27 +21,61 @@ typedef struct {
     char aniversario[10];
 } Contato;
 
-void inserirContato() {}
+void inserirContato(Contato *contato)
+{
+    printf("Digite o nome: ");
+    scanf("%s", contato->nome);
+    printf("Digite o telefone: ");
+    scanf("%s", contato->telefone);
+    printf("Digite o celular: ");
+    scanf("%s", contato->celular);
+    printf("Digite o endereco: ");
+    scanf("%s", contato->endereco);
+    printf("Digite o aniversario: ");
+    scanf("%s", (*contato).aniversario);
+}
 
-void visualizar() {}
+void visualizar(Contato *contato)
+{
+    printf("Nome: %s\n", contato->nome);
+    printf("Telefone: %s\n", contato->telefone);
+    printf("Celular: %s\n", contato->celular);
+    printf("Endereco: %s\n", contato->endereco);
+    printf("Aniversario: %s\n", contato->aniversario);
+}
 
 int main()
 {
-    void *opcoes[3] = { "1 - inserir", "2 - visualizar", "3 - sair" };
-    int opcao = 0;
-    Contato contatos[MAX];
+    void *opcoes[3] = {
+        "1 - inserir",
+        "2 - visualizar",
+        "3 - sair"};
+    int opcao = 0, i = 0, position = 0, a = 0;
+    Contato *contatos[MAX];
 
     do {
+        for (a = 0; a < 3; a++)
+        {
+            printf("%s\n", opcoes[a]);
+        }
         scanf("%d", &opcao);
         switch(opcao) {
             case 1:
-                inserirContato();
-            break;
+                contatos[i] = (Contato *)malloc(sizeof(Contato));
+                inserirContato(contatos[i]);
+                i++;
+                printf("Adicionado com sucesso!\n");
+                break;
             case 2:
-                visualizar();
-            break;
+                printf("Digite a posicao: ");
+                scanf("%d", &position);
+                visualizar(contatos[position]);
+                break;
             default:
                 opcao = 3;
         }
     } while(opcao != 3);
+    for (a = 0; a < MAX; a++)
+        if (contatos[a] != 0)
+            free(contatos[a]);
 }
